a71360d7ef075754c523921b7b45f372caf26193
g-sasha <sasha_rotax@mail.ru>
2020-06-05 12:55:13 +0300

    Создали переменные и вывели их в консоль

--- js/main.js
+++ js/main.js
@@ -1,12 +1,13 @@
+let money = 30000,
+    income = 'Фриланс',
+    addExpenses = 'Ипотека, комуналка, бензин',
+    deposit = true,
+    mission = 300000,
+    period = 12;     
 
-let money,
-    income,
-    addExpenses,
-    deposit,
-    mission,
-    period;
+console.log(typeof money);
+console.log(typeof income);
+console.log(typeof deposit);
 
-alert ('Hello World');
 
-console.log('Welcome my Friend');
-    
+
