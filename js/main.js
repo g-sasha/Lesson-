f1483b56407d442a5852a19f6876075f70556179
g-sasha <sasha_rotax@mail.ru>
2020-06-05 15:11:57 +0300

    сделал усложненное задание

--- js/main.js
+++ js/main.js
@@ -1,31 +1,17 @@
-let money = 30000,
-    income = 'Фриланс',
-    addExpenses = 'Ипотека, Комуналка, Бензин',
-    deposit = true,
-    mission = 300000,
-    period = 12;  
+let num = 266219;
 
-// Вывели тип значений в консоль
-console.log(typeof money); 
-console.log(typeof income);
-console.log(typeof deposit);
+// создали массив 
+let nums =(('' + num).split(''));
+let number = 1;
+console.log(nums);
 
-// Вывели длинну строки в консоли 
-console.log (addExpenses.length);
+// Произвели умножение массива
+nums.forEach(function(e) {number *= e;});
+console.log(number);
 
-// Вывели в консоль фразу
-console.log('Период равен ' + period + ' месяцев');
-console.log('Цель заработать ' + mission + ' рублей');
+// Возвели в степень
+console.log(number ** 2);
 
-// Привели строку к нижнему регистру и разбили на массив
-console.log(addExpenses.toLowerCase());
-
-console.log(addExpenses.split(','));
-
-// Вычисляем дневной бюджет
-let budgetDay,
-    incomeMoney = 5000;
-budgetDay =(incomeMoney + money) /30;
-console.log(budgetDay.toFixed(2));
-
+// Оставили первые 2 числа 
+console.log(String(number ** 2).slice(0,2));
 
