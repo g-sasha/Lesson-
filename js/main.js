8884b287688ff0a92f421e71ac4476d41143d929
g-sasha <sasha_rotax@mail.ru>
2020-06-09 22:21:21 +0300

    Сделал домашнее задание

--- js/main.js
+++ js/main.js
@@ -28,4 +28,46 @@ let budgetDay,
 budgetDay =(incomeMoney + money) /30;
 console.log(budgetDay.toFixed(2));
 
+// Выводим месячный доход в выплывающее окно (number)
+money = +prompt('Ваш месячный доход?');
+console.log(money);
 
+// перчечисляем расходы (string)
+addExpenses = prompt('Перечислите возможные расходы за рассчитываемый период через запятую');
+console.log(addExpenses.split(','));
+
+// Депозит (boolean)
+deposit  = confirm ('Есть ли у вас депозит в банке?')
+console.log(deposit);
+
+
+let expenses1 = prompt ('Введите обязательную статью расходов?');
+
+let amount1 = +prompt (' Во сколько Вам это обойдется?')
+
+let expenses2 = prompt ('Введите обязательную статью расходов?');
+
+let amount2 = +prompt (' Во сколько Вам это обойдется?')
+
+// Бюджет на месяц
+let budgetMonth = (money - (amount1 - amount2))
+console.log ('Бюджет на месяц: ' + budgetMonth);
+
+// Достижение цели 
+console.log ('Цель достигнуть за: ' + (Math.ceil( mission / budgetMonth)));
+
+// Бюджет на день
+budgetDay = budgetMonth/30;
+console.log('бюджет на день: ' + (budgetDay.toFixed(2)));
+
+
+// Уровень дохода
+if (budgetDay >= 1200){
+    console.log('У вас высокий уровень дохода');
+ } else if (budgetDay <= 1200 && budgetDay >= 600){
+  console.log(' У вас средний уровень доходамально ');
+ } else if (budgetDay < 600 && budgetDay > 0){
+  console.log(' К сожалению у вас уровень дохода ниже среднего ');
+ } else if (budgetDay < 0){
+  console.log(' Что то пошло не так ');
+ }
