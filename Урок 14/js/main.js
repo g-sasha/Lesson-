e6d988a72cd57527ef6f3ab29e35f569abd8130b
g-sasha <sasha_rotax@mail.ru>
2020-07-03 10:30:39 +0300

    Исправил 14-1

--- Урок 14/js/main.js
+++ Урок 14/js/main.js
@@ -1,40 +1,37 @@
 'use strict';
 
-function DomElement (selector, height, width, bg, fontSize){
+function DomElement (selector, height, width, bg, fontSize, text){
     this.selector = selector;
     this.height = height;
     this.width = width;
     this.bg = bg;
     this.fontSize = fontSize;
+    this.text = text;
 }
-
-let domElem = new DomElement ('.mazda', '1.6', '2.4', 'red', '16');
-console.log('domElem: ', domElem);
 
 let src = document.getElementsByTagName('script');
 
-DomElement.prototype.elem = function(){
-    if(domElem.selector.substring(0 , 1) === '.'){
-        let newDiv = document.createElement('div');
-        newDiv.classList.add('block');
-        console.log(newDiv);
-        newDiv.style.cssText = `
-        height: 20px;
-        width: 50px;
-        background: red;
-        font-size: 16px;
-        `;
-        newDiv.innerHTML = '<span> Привет </span> ';
-        document.body.insertAdjacentElement('afterend', newDiv);
-    }  else if (domElem.selector.substring(0 , 1) === '#') {
-        let newId = document.createElement('div');
-        newId.id = 'best';
-        console.log('newId: ', newId);
+DomElement.prototype.createElement = function(){
+    let newDiv;
+    if(this.selector.substring(0 , 1) === '.'){
+        newDiv = document.createElement('div');
+        newDiv.classList.add(this.selector);
+    }  else if (this.selector.substring(0 , 1) === '#') {
+        newDiv = document.createElement('div');
+        newDiv.id = this.selector;
     } 
-    
+        newDiv.style.cssText = `
+        height: ${this.height}px;
+        width: ${this.width}px;
+        background-color : ${this.bg};
+        font-size: ${this.fontSize}px;
+        `;
+        newDiv.textContent = `${this.text}`;
+        document.body.insertAdjacentElement('afterend', newDiv);
 };
-domElem.elem();
 
+let domElem = new DomElement ('#mazda', '100', '150', 'red', '16', 'привет');
+domElem.createElement();
 
 
 let newDomElement = Object.create(DomElement);
